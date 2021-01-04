abe745174630df266941d5eea9067335dd33da07
Basel <mrbasel555@gmail.com>
2021-01-04 10:11:23 +0300

    Implement websockets in frontend

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,22 @@
 window.onload = () => {
+    let socket = io.connect('http://127.0.0.1:5000');
+
+    socket.on("connect", () => {
+        console.log("You are connected.");
+    })
+
+    socket.on("newData", (cordinates) => {
+        ctx.beginPath();
+        for (let index = 0; index < cordinates.length; index++) {
+            ctx.lineTo(cordinates[index].xCord, cordinates[index].yCord);
+            ctx.stroke();
+        }
+    })
+
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
     let isMouseDown = false;
+    let cords = [];
 
     // Set canvas to fullscreen
     canvas.width = window.innerWidth;
@@ -19,6 +34,11 @@ window.onload = () => {
         if (isMouseDown) {
             ctx.lineTo(x.clientX, x.clientY);
             ctx.stroke();
+
+            cords.push({
+                xCord: x.clientX,
+                yCord: x.clientY
+            });
         }
     })
 
@@ -26,5 +46,8 @@ window.onload = () => {
         isMouseDown = false;
         ctx.lineTo(x.clientX, x.clientY);
         ctx.stroke();
+
+        socket.emit("mouseEvent", cords);
+        cords = [];
     })
 }
