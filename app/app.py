c52040af61cbc89010826583adbd6b2e0cbaf4a5
Basel <mrbasel555@gmail.com>
2021-03-19 23:51:23 +0300

    Refactor

--- app/app.py
+++ app/app.py
@@ -3,18 +3,19 @@ import secrets
 import time
 
 from flask import Flask, request, render_template, url_for, redirect, flash
-from redis import Redis
 from redis.exceptions import ConnectionError
 from flask_socketio import SocketIO, emit, leave_room
 from flask_socketio import join_room as join_socket_room
 from dotenv import load_dotenv
+
+from db import RedisDbWrapper
 
 load_dotenv()
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 socketio.init_app(app, cors_allowed_origins="*")
-db = Redis()
+db = RedisDbWrapper()
 
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
@@ -27,7 +28,7 @@ def home():
 @app.route("/room/<room_id>", methods=["GET"])
 def draw_room(room_id):
     try:
-        rooms = [i.decode("utf-8") for i in db.lrange("rooms", 0, -1)]
+        rooms = db.get_rooms()
     except ConnectionError:
         flash("Something went wrong, please try again later.", "error")
         return redirect(url_for("home"))
@@ -44,7 +45,7 @@ def create_room_form():
         room_id = secrets.token_hex(8)
 
         try:
-            rooms = [i.decode("utf-8") for i in db.lrange("rooms", 0, -1)]
+            rooms = db.get_rooms()
         except ConnectionError:
             flash("Something went wrong, please try again later.", "error")
             return redirect(url_for("home"))
@@ -52,7 +53,7 @@ def create_room_form():
         while room_id in rooms:
             room_id = secrets.token_hex(8)
 
-        db.rpush("rooms", room_id)
+        db.create_room(room_id)
 
         return redirect(url_for("draw_room", room_id=room_id))
     else:
@@ -63,29 +64,27 @@ def create_room_form():
 def on_client_disconnect(data):
     room_id = data.get("roomId")
 
-    # Remove user's id from db
-    db.lrem(f"{room_id}:users_ids", 1, request.sid)
-    room_users_count = db.lrange(f"{room_id}:users_ids", 0, -1)
+    db.remove_user(room_id, request.sid)
+    room_users_ids = db.get_room_users(room_id)
 
-    if len(room_users_count) == 0:
-        db.delete(f"{room_id}:users_ids")
-        db.lrem("rooms", 1, room_id)
+    if len(room_users_ids) == 0:
+        db.delete_room(room_id)
 
 
 @socketio.on("joinRoom")
-def join_room(room):
-    join_socket_room(room)
+def join_room(room_id):
+    join_socket_room(room_id)
 
-    room_users_ids = [i.decode("utf-8") for i in db.lrange(f"{room}:users_ids", 0, -1)]
-    db.rpush(f"{room}:users_ids", request.sid)
+    room_users = db.get_room_users(room_id)
+    db.add_user(room_id, request.sid)
 
-    if len(room_users_ids) >= 1:
+    if len(room_users) >= 1:
         emit(
             "getCanvasImage",
             request.sid,
             broadcast=False,
             include_self=False,
-            room=room_users_ids[0],
+            room=room_users[0],
         )
 
 
@@ -132,7 +131,7 @@ def handle_erase_event(data):
 @socketio.on("clearEvent")
 def clear_canvas(data):
     room_id = data.get("room")
-    db.delete(f"{room_id}:image")
+    db.clear_board_state(room_id)
 
     emit("clearEvent", broadcast=True, include_self=False, room=room_id)
 
